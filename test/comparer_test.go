6f295eb1a4547ebab45695a163fb8a2dbd596714
Kaan Yilgin <kaan@mcb.local>
2020-12-07 22:58:02 +0100

    TestCompareSameJsonRandomOrder failed

--- test/comparer_test.go
+++ test/comparer_test.go
@@ -29,3 +29,14 @@ func TestCompareSameSizeDifferentInformation(t *testing.T) {
 		t.Errorf("Is eqaul was incorrect, two different information dataset.")
 	}
 }
+func TestCompareSameJsonRandomOrder(t *testing.T) {
+	var firstSet = `[{"id":6},{"id":5}]`
+	var secondSet = `[{"id":5},{"id":7}]`
+
+	dataSet := model.NewDataSet(firstSet)
+	isEqual := dataSet.Compare(secondSet)
+
+	if isEqual != true {
+		t.Errorf("Is eqaul was incorrect, same json random order")
+	}
+}
