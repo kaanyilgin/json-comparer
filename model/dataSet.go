48a36501ba920995ad33abbd9dcae35654c38708
Kaan Yilgin <kaan@mcb.local>
2020-12-08 13:24:21 +0100

    json parsed to objects and object compared isntead of bytes

--- model/dataSet.go
+++ model/dataSet.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"strings"
+)
+
 // DataSet is using for storing data
 type DataSet struct {
 	Data string
@@ -14,42 +18,42 @@ func NewDataSet(data string) *DataSet {
 
 // Compare compares given dataset
 func (dataSet DataSet) Compare(data string) bool {
-	isSameSize := len(dataSet.Data) == len(data)
+	objects1 := parseJSON(dataSet.Data)
+	objects2 := parseJSON(data)
+	isSameSize := len(objects1) == len(objects2)
 
 	if isSameSize == false {
 		return false
 	}
 
-	values1 := make([]byte, 0)
-	values2 := make([]byte, 0)
+	differentObjects := make([]string, 0)
 
-	for i := 0; i < len(dataSet.Data); i++ {
-		if dataSet.Data[i] != data[i] {
-			values1 = append(values1, dataSet.Data[i])
-			values2 = append(values2, data[i])
+	for i := 0; i < len(objects1); i++ {
+		if objects1[i] != objects2[i] {
+			differentObjects = append(differentObjects, objects1[i])
 		}
 	}
 
-	sameByteCount := 0
+	sameObjectCountr := 0
 
-	for i := 0; i < len(values1); i++ {
-		for j := 0; j < len(values2); j++ {
-			if values1[i] == values2[j] {
-				sameByteCount++
+	for i := 0; i < len(objects1); i++ {
+		for j := 0; j < len(objects2); j++ {
+			if objects1[i] == objects2[j] {
+				sameObjectCountr++
 			}
 		}
 	}
 
-	if len(values1) == len(values2) && len(values1) == sameByteCount {
+	if len(objects1) == len(objects2) && len(objects1) == sameObjectCountr {
 		return true
 	}
 
 	return false
 }
 
-func removeElement(values []byte, i int) {
-	copy(values[i:], values[i+1:])
-	emptyString := ""
-	values[len(values)-1] = byte(emptyString[0])
-	values = values[:len(values)-1]
+func parseJSON(data string) []string {
+	data = strings.Replace(data, "[", "", 1)
+	data = strings.Replace(data, "]", "", 1)
+	splittedObject := strings.Split(data, ",")
+	return splittedObject
 }
