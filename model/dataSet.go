ea574b27bf3f35058a453ae2b22d1a33b9c96bfb
Kaan Yilgin <kaan@mcb.local>
2020-12-08 15:21:13 +0100

    parsing json in constructor

--- model/dataSet.go
+++ model/dataSet.go
@@ -6,21 +6,20 @@ import (
 
 // DataSet is using for storing data
 type DataSet struct {
-	Data string
+	objects []string
 }
 
 // NewDataSet creates a new DataSet object
 func NewDataSet(data string) *DataSet {
 	return &DataSet{
-		Data: data,
+		objects: ParseJSON(data),
 	}
 }
 
 // Compare compares given dataset
 func (dataSet DataSet) Compare(data string) bool {
-	objects1 := parseJSON(dataSet.Data)
 	objects2 := parseJSON(data)
-	isSameSize := len(objects1) == len(objects2)
+	isSameSize := len(dataSet.objects) == len(objects2)
 
 	if isSameSize == false {
 		return false
@@ -28,23 +27,23 @@ func (dataSet DataSet) Compare(data string) bool {
 
 	differentObjects := make([]string, 0)
 
-	for i := 0; i < len(objects1); i++ {
-		if objects1[i] != objects2[i] {
-			differentObjects = append(differentObjects, objects1[i])
+	for i := 0; i < len(dataSet.objects); i++ {
+		if dataSet.objects[i] != objects2[i] {
+			differentObjects = append(differentObjects, dataSet.objects[i])
 		}
 	}
 
 	sameObjectCountr := 0
 
-	for i := 0; i < len(objects1); i++ {
+	for i := 0; i < len(dataSet.objects); i++ {
 		for j := 0; j < len(objects2); j++ {
-			if objects1[i] == objects2[j] {
+			if dataSet.objects[i] == objects2[j] {
 				sameObjectCountr++
 			}
 		}
 	}
 
-	if len(objects1) == len(objects2) && len(objects1) == sameObjectCountr {
+	if len(dataSet.objects) == len(objects2) && len(dataSet.objects) == sameObjectCountr {
 		return true
 	}
 
---
+++ model/jsonParser.go
@@ -0,0 +1,11 @@
+package model
+
+import "strings"
+
+// ParseJSON parse json into a dynamic object
+func ParseJSON(data string) []string {
+	data = strings.Replace(data, "[", "", 1)
+	data = strings.Replace(data, "]", "", 1)
+	splittedObject := strings.Split(data, ",")
+	return splittedObject
+}
