57dfe6945785225f7c68934348a48d156ff074e3
Kaan Yilgin <kaan@mcb.local>
2020-12-11 13:34:41 +0100

    banchmarking LoopingTwoObjectAttributeComparer vs LoopingTwoObjectAttributeComparer

--- model/comparer_banchmark_test.go
+++ model/comparer_banchmark_test.go
@@ -8,14 +8,37 @@ import (
 
 var (
 	dataSetComparer = new(LoopingTwoDataSetComparer)
-	objectComparer  = LoopingTwoObjectAttributeComparer{}
+	reader          = &infrastructure.FileReader{}
+	jsonParser      = newSystemJSONParser()
 )
 
 func BenchmarkWithoutAttributeSorting(b *testing.B) {
-	reader := &infrastructure.FileReader{}
-	jsonParser := newSystemJSONParser()
-	dataset := NewDataSet("MOCK_DATA.json", reader, dataSetComparer, objectComparer, jsonParser)
-	dataset2 := NewDataSet("MOCK_DATA.json", reader, dataSetComparer, objectComparer, jsonParser)
+	objectComparer := &LoopingTwoObjectAttributeComparer{}
+	mockReader1 := &mockDataReader{
+		data: `[{"last_name":"Dumbreck","ip_address":"206.54.46.237","id":1,"gender":"Male","first_name":"Brannon","email":"bdumbreck0@flavors.me","country_code":"Serbia","country":"Ecuador","city":"Babahoyo","animal_name":"Indian leopard"}]`,
+	}
+	mockReader2 := &mockDataReader{
+		data: `[{"id":1,"first_name":"Brannon","last_name":"Dumbreck","email":"bdumbreck0@flavors.me","gender":"Male","ip_address":"206.54.46.237","animal_name":"Indian leopard","city":"Babahoyo","country":"Ecuador","country_code":"Serbia"}]`,
+	}
+	dataset := NewDataSet("", mockReader1, dataSetComparer, objectComparer, jsonParser)
+	dataset2 := NewDataSet("", mockReader2, dataSetComparer, objectComparer, jsonParser)
+
+	isEqual, _ := dataset.Compare(dataset2)
+
+	print(isEqual)
+}
+
+func BenchmarkWithDirectlyFindAttributeKeyByMapKey(b *testing.B) {
+	objectComparer := &FindAttributeByKeyObjectComparer{}
+	mockReader1 := &mockDataReader{
+		data: `[{"last_name":"Dumbreck","ip_address":"206.54.46.237","id":1,"gender":"Male","first_name":"Brannon","email":"bdumbreck0@flavors.me","country_code":"Serbia","country":"Ecuador","city":"Babahoyo","animal_name":"Indian leopard"}]`,
+	}
+	mockReader2 := &mockDataReader{
+		data: `[{"id":1,"first_name":"Brannon","last_name":"Dumbreck","email":"bdumbreck0@flavors.me","gender":"Male","ip_address":"206.54.46.237","animal_name":"Indian leopard","city":"Babahoyo","country":"Ecuador","country_code":"Serbia"}]`,
+	}
+
+	dataset := NewDataSet("", mockReader1, dataSetComparer, objectComparer, jsonParser)
+	dataset2 := NewDataSet("", mockReader2, dataSetComparer, objectComparer, jsonParser)
 
 	isEqual, _ := dataset.Compare(dataset2)
 
--- model/objectComparer.go
+++ model/objectComparer.go
@@ -26,3 +26,25 @@ func (l LoopingTwoObjectAttributeComparer) Compare(firstJSONObject JSONObject, s
 
 	return true, nil
 }
+
+//FindAttributeByKeyObjectComparer compares two objects going through whole attribute
+type FindAttributeByKeyObjectComparer struct {
+	ObjectComparer
+}
+
+//Compare compares two object
+func (f FindAttributeByKeyObjectComparer) Compare(firstJSONObject JSONObject, secondJSONObject JSONObject) (bool, error) {
+	if len(firstJSONObject.attributes) != len(secondJSONObject.attributes) {
+		return false, nil
+	}
+
+	for key, attribute := range firstJSONObject.attributes {
+		comparedValue := secondJSONObject.attributes[key]
+
+		if attribute != comparedValue {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
